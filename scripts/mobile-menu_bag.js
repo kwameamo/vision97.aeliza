ca14777a8f94b67a009d69db1a7cdd0ee8bbc00f
kwameamo <118262204+kwameamo@users.noreply.github.com>
2022-12-27 16:20:55 +0000

    added product img swap on hover and full page menu

---
+++ scripts/mobile-menu_bag.js
@@ -0,0 +1,18 @@
+/* Mobile Menus*/ 
+
+  /* Open nav */
+  function openMenu() {
+      document.getElementById("mob-menu").style.visibility = "visible";
+    }
+
+    /* Close nav */
+  function closeMenu() {
+      document.getElementById("mob-menu").style.visibility = "hidden"; 
+    }
+
+
+  
+
+
+
+
